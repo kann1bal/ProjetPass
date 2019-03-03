d81cc9f2a379c304593f949709b940f019488408
kann1bal <mohamedzied.barketallah@esprit.tn>
2019-03-04 00:43:35 +0100

    first commit

---
+++ ProjetPi/Solution.Data/MyContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solution.Data
+{
+    public class MyContext : DbContext
+    {
+        public MyContext():base("name=machaine")
+        {
+
+        }
+        // dbset
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //modelBuilder.Configurations.Add();
+            //modelBuilder.Conventions.Add();
+        }
+    }
+}
